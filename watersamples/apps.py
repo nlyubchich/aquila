80df9d6f718065169392cbac0ec1631d6ca84287
Nikita Lyubchich <nikita.lyubchich@gmail.com>
2017-04-04 14:03:30 +0300

    initial django

---
+++ watersamples/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WatersamplesConfig(AppConfig):
+    name = 'watersamples'
---
+++ watersamples/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Member(models.Model):
+    """A model of a rock band member."""
+    name = models.CharField("Member's name", max_length=200)
+    instrument = models.CharField(choices=(
+            ('g', "Guitar"),
+            ('b', "Bass"),
+            ('d', "Drums"),
+        ),
+        max_length=1
+    )
+    band = models.ForeignKey("Band")
