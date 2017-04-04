2b833d7577d6d2736fa587c4bc6194f254fdf216
Nikita Lyubchich <nikita.lyubchich@gmail.com>
2017-04-04 17:06:11 +0300

    Fixed production db settings

--- aquila/settings/production.py
+++ aquila/settings/production.py
@@ -5,7 +5,6 @@ SECRET_KEY = os.environ['APP_SECRET_KEY']
 DEBUG = False
 ALLOWED_HOSTS = [os.environ['APP_ALLOWED_HOST']]
 
-DATABASES = {}
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=500)
+}
